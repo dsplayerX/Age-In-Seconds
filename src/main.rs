9781abae5a56d558d5924c9b203f6b293af4254c
dsplayerX <dumindu0043@gmail.com>
2022-07-13 23:21:02 +0530

    testing cargo module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use chrono::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let current_time = Local::now();
+
+    let birthday = Local.ymd(1995, 05, 25).and_hms(3, 15, 00);
+
+    let time_passed: i64 = current_time.timestamp() - birthday.timestamp();
+
+    
+    println!("{}", current_time.month() as i64);
+    println!("{}", birthday.month() as i64);
+    println!("Age In Seconds: {}", time_passed);
+
 }
